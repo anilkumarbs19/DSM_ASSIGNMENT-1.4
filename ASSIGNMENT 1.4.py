6ea572b4d2baa50b147a6cee441b4db2049155c9
anilkumarbs19 <anilkumarbs19@gmail.com>
2018-02-05 21:30:29 +0530

    Assignment 1.4 DSM

---
+++ ASSIGNMENT 1.4.py
@@ -0,0 +1,13 @@
+
+# coding: utf-8
+
+# In[14]:
+
+
+diameter = 12            #diameter is given in the problem statement
+radius = diameter/2      #formula for finding radius  
+pi =3.14
+volume = 4/3 * pi * (radius)**3
+print("Volume of the Sphere is :")
+print(volume)
+
